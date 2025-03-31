fe82a6ae6d11e60691edaf945418ba68a7f40e1c
ianconnon <ianvconnong@gmail.com>
2025-03-31 12:13:06 -0300

    Entrega del tp

--- change.py
+++ change.py
@@ -1,3 +1,11 @@
 def change():
     expense = 23.75
     money = 100
+    turned = money - expense
+    turned_int = int(turned)
+    turned_d = int((turned-turned_int) * 100)
+
+    print(f'Ingresar gasto:\n{expense}\nDinero recibido\n{money}\n\nVuelto\n\nPesos:\n{turned_int}\nCentavos:\n{turned_d}')
+    
+
+change()
--- earth.py
+++ earth.py
@@ -1,3 +1,7 @@
 def earth():
     x = "Bangladesh"
     y = "Barbados"
+    print('The result of Bangladesh comes first in the dictionary than Barbados is True.')
+    print('The result of Barbados comes first in the dictionary than Bangladesh is False.')
+
+earth()
